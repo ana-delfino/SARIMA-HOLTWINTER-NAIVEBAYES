81cbe913f0eb88c62db9dcb6411360c540bb6133
Ana Paula Santos Delfino <57999613+ana-delfino@users.noreply.github.com>
2022-01-30 11:04:25 -0300

    add description

--- modelos.R
+++ modelos.R
@@ -1,4 +1,13 @@
 ######Consumo Livre#######
+## SARIMA-HOLTWINTER-NAIVEBAYES
+
+###### Comparação entre os modelos SARIMA, Naive e Winters Aditivo para previsão do consumo de energia elétrica no mercado livre de energia brasileiro.
+
+
+###### O planejamento energético é uma ferramenta essencial para compatibilizar oferta e demanda de energia. Neste contexto, a estimação do consumo de energia é um parâmetro importante para os policy makers, especialmente em termos de complementação da geração. Este trabalho compara a acuraria de previsão do consumo no ambiente de contratação livre do setor elétrico utilizando do modelo SARIMA (Seasonal Autoregressive Integrated Moving Average), o modelo Naive e o modelo de Winters Aditivo. Como resultado este trabalho mostra que dentre os modelos analisados, o modelo de Winters Aditivo faz uma previsão mais acurada, apresentando um MAPE e um RMSE menor, entretanto o modelo a tem problemas em representar as características estatísticas da série histórica. 
+
+###### Trabalho desenvolvido durante o mestrado na Disciplina de Métodos Bayesianos de Previsão.
+
 
 setwd ("C:\\Users\\Ana\\Documents\\PUC\\Inferencia bayesiana\\Trabalho\\Base")
 #Bibliotecas#
